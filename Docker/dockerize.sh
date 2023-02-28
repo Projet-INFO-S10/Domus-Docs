89bcf918d052bf1fdcc380369200b9808c9c2338
glavirotte <gaetan@lavirotte.com>
2023-02-28 10:54:02 +0100

    Change appartement directory git

--- Docker/dockerize.sh
+++ Docker/dockerize.sh
@@ -2,8 +2,7 @@
 if [ -x "$(command -v docker)" ]; then  # Check if docker is installed
     if git clone git@gricad-gitlab.univ-grenoble-alpes.fr:plateforme-domus/appartement.git; then # Check if git clone is successful
         echo "git clone success !"
-        mkdir openhab_addons openhab_conf openhab_userdata # Create the folders for the docker container
-        mv appartement/* openhab_conf   # Move the files into the right folder
+        mkdir openhab_addons openhab_userdata # Create the folders for the docker container
 
         echo "Setting up openhab into a docker container..."
         docker-compose up -d # Start the docker container
