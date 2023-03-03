388c49bd7b3f3a22499ef4b1bfab7a78cbf2f289
Cyprien <cyprien.pelisse.verdoux15@gmail.com>
2023-03-03 14:23:27 +0100

    New script to test

--- Meross/dockerize.sh
+++ Meross/dockerize.sh
@@ -1,27 +1,61 @@
-#!/bin/sh
-if [ -x "$(command -v docker)" ]; then  # Check if docker is installed
-    echo "Setting up openhab into a docker container..."
-    bash key_generator.sh
-    docker-compose up -d # Start the docker container
-    echo "The MQTT Broker is now running in a docker container"
+#!/bin/bash
 
-    git clone https://github.com/bytespider/Meross
-    cd Meross
-    npm i
+echo "Welcome in the Meross configuration script "
+echo "Cloning the repository"
 
-    echo "The next step requires you to connect to the WiFi of your Meross device, so please do it before continuing."
-
-    echo "Please enter the SSID of your WiFi network: "
-    read SSID
-    echo "Please enter the password of your WiFi network: "
-    read PASSWORD
-    echo "Setting up WiFi connection..."
-
-    cd bin
-    ./meross setup --gateway 10.10.10.1 --wifi-ssid $SSID --wifi-pass $PASSWORD --mqtt mqtts://172.25.0.2:8883
-
-    echo "The Meross device is now connected to your WiFi network and the MQTT broker, you can switch back to your normal WiFi network."
-
-else
-    echo "Please Install docker and docker-compose"
+DIRECTORY=Meross
+if [ ! -d "$DIRECTORY" ]; then
+  echo "Directory doesn't exist"
+  git clone https://github.com/bytespider/Meross
 fi
+
+cd $DIRECTORY
+
+if [ -x "$(command -v npm)" ]; then  # Check if npm is installed
+    npm i
+    echo "Now, change the network your connected at to get connected to Meross outlet network "
+
+    echo "Do you want to continue ? [Y/N]"
+    read mot
+    while
+    [ "$mot" = "Y" ]
+    do
+    echo "Do you want to continue ? [Y/N]"
+    read mot
+    done
+
+    echo "Welcome in the part 2 of the script."
+    echo "Be sure that the outlet is set to default configuration" 
+    echo "To reset to factory default the outlet, hold the button down for 5s"
+
+    echo "Do you want to continue ? [Y/N]"
+    read mot
+    while
+    [ "$mot" = "Y" ]
+    do
+    echo "Do you want to continue ? [Y/N]"
+    read mot
+    done
+
+    gateway=10.10.10.1
+
+    echo "What is the SSID that the outlet need to connect to ? "
+    read ssid
+    echo "What is the password that the outlet need to connect to ? "
+    read password
+
+    echo "now, connect to the network where OpenHab is, and press Y to continue"
+    read mot
+    while
+    [ "$mot" = "Y" ]
+    do
+    echo "now, connect to the network where OpenHab is, and press an touch to continue"
+    read mot
+    done
+
+    ./meross setup --gateway $gateway --wifi-ssid $ssid --wifi-pass $password --mqtt mqtts://172.25.0.2:8883
+else
+    echo "Please Install npm"
+    echo "Look at: https://docs.npmjs.com/cli/v7/configuring-npm/install "
+fi
+
