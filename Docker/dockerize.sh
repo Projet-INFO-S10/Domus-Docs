08e5f861ef9cbfdd48e2a2cf6836efaed03ec2b5
glavirotte <gaetan@lavirotte.com>
2023-03-08 15:42:22 +0100

    Fix bugs

--- Docker/dockerize.sh
+++ Docker/dockerize.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 if [ -x "$(command -v docker)" ]; then  # Check if docker is installed
-    if git clone git@gricad-gitlab.univ-grenoble-alpes.fr:plateforme-domus/appartement.git; then # Check if git clone is successful
-        echo "git clone success !"
-        mkdir openhab_addons openhab_userdata # Create the folders for the docker container
+    DIRECTORY=appartement
+    
+    if [ ! -d "$DIRECTORY" ]; then
+        if git clone git@gricad-gitlab.univ-grenoble-alpes.fr:plateforme-domus/appartement.git; then # Check if git clone is successful
+            echo "git clone success !"
+            mkdir openhab_addons openhab_userdata # Create the folders for the docker container
 
-        echo "Setting up openhab into a docker container..."
-        docker-compose up -d # Start the docker container
-    else
-        echo "git clone failed ! Please check that you have the right to clone the repository"
+            echo "Setting up openhab into a docker container..."
+            docker-compose up -d # Start the docker container
+        else
+            echo "git clone failed ! Please check that you have the right to clone the repository"
+        fi
     fi
 else
     echo "Please Install docker and docker-compose"
